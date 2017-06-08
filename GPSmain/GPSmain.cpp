0169fb9d9718a589fa61ec266aff92f4e3f2a47d
Motohiro Shibakawa <c201075@gmail.com>
2017-06-08 19:58:23 +0900

    OpenCV3.1.0とサンプルプロジェクト追加

--- GPSmain/GPSmain.cpp
+++ GPSmain/GPSmain.cpp
@@ -3,9 +3,48 @@
 
 #include "stdafx.h"
 
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/features2d.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
+
+	//カレントディレクトリはデフォルトでC:\Users\｛ユーザ名｝\Documents\Visual Studio 2015\Projects\GPSmain\GPSmain
+	string name = "p.jpg";
+	//string name = C:\\temp\\p.jpg
+
+	Mat image;
+	image = imread(name, IMREAD_COLOR);
+
+	//正しく読み込まれているか？
+	if (!image.data) {
+		return -1;
+	}
+
+	//アルゴリズムにAKAZEを使用する
+	auto algorithm = cv::AKAZE::create();
+
+	// 特徴点抽出
+	vector<cv::KeyPoint> keypoints;
+	algorithm->detect(image, keypoints);
+
+	Mat output;
+	drawKeypoints(image, keypoints, output);
+
+
+	namedWindow("test", WINDOW_AUTOSIZE);
+	imshow("test", output);
+
+	waitKey(0);
+
+	return 0;
 }
 
